9a92b129e050546d3c14007b6f3b2316f9d27d75
achillescosmo <49824563+achillescosmo@users.noreply.github.com>
2021-12-13 22:11:19 +0700

    finsh-project

--- credentials.js
+++ credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
     "cookieSecret": "Dio Santissimo Misericordia de Mi",
     "mongo": {
-        "connectionString": "mongodb://localhost:27017/myapp"
+        "connectionString": "mongodb+srv://admin:123@cluster0.v3jqq.mongodb.net/myapp?retryWrites=true&w=majority"
     }
 }
